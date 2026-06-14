68e42feaf5e3ebc831068d74295d997e8e5049e6
Matthew Whitehorn <whitehornmatthew@gmail.com>
2026-06-14 11:12:03 -0400

    Filtering product functionality works

--- src/specs/productTest.spec.ts
+++ src/specs/productTest.spec.ts
@@ -1,8 +1,67 @@
-import homePage from "../pages/homePage";
-import cartPage from "../pages/cartPage";
 import loginPage from "../pages/logInPage";
 import testData from "../data/testData.json";
+import productPage from "../pages/productPage";
 
+const productGroups = [
+    {
+        label: "all",
+        names: [
+            testData.products.automationHandbook.name,
+            testData.products.sauceLabsBackpack.name,
+            testData.products.sauceLabsBikeLight.name,
+            testData.products.boltTShirt.name,
+            testData.products.debugSocks.name,
+            testData.products.sauceLabsFleeceJacket.name,
+            testData.products.sauceLabsHoodie.name,
+            testData.products.sauceLabsOnesie.name,
+            testData.products.testAllTheThingsTShirt.name,
+            testData.products.labNotebook.name,
+            testData.products.qaStickerPack.name,
+            testData.products.sauceLabsMug.name,
+        ],
+    },
+    {
+        label: "apparel",
+        names: [
+            testData.products.testAllTheThingsTShirt.name,
+            testData.products.sauceLabsFleeceJacket.name,
+            testData.products.boltTShirt.name,
+            testData.products.sauceLabsOnesie.name,
+            testData.products.sauceLabsHoodie.name,
+        ],
+    },
+    {
+        label: "accessory",
+        names: [
+            testData.products.automationHandbook.name,
+            testData.products.debugSocks.name,
+            testData.products.sauceLabsMug.name,
+            testData.products.qaStickerPack.name,
+        ],
+    },
+    {
+        label: "gear",
+        names: [
+            testData.products.sauceLabsBackpack.name,
+            testData.products.sauceLabsBikeLight.name,
+        ],
+    },
+    {
+        label: "book",
+        names: [
+            testData.products.labNotebook.name,
+        ],
+    },
+] as const;
+
+const assertProductsInContainer = async (productNames: readonly string[]): Promise<void> => {
+    await expect(productPage.productContainer).toBeDisplayed();
+
+    for (const productName of productNames) {
+        await expect(productPage.productContainer).toHaveText(productName, { containing: true });
+    }
+};
+    
 describe("Product Page Functionality", () => {
 
     beforeEach(async () => {
@@ -10,7 +69,9 @@ describe("Product Page Functionality", () => {
         await loginPage.successfulLoginUser(testData.users.swiftTester.username, testData.users.swiftTester.password);
     });
 
-    it("Product page shows all products correctly", async () => {
-        
-    });
+    for (const group of productGroups) {
+        it(`Product page shows all ${group.label} products correctly`, async () => {
+            await assertProductsInContainer(group.names);
+        });
+    }
 });
